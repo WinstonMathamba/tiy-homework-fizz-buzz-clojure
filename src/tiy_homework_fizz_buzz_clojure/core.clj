6798d37c31c90edaa706c9b25d1b2b4a32e98979
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-10 16:21:26 -0400

    Update that fixed nil issue

--- src/tiy_homework_fizz_buzz_clojure/core.clj
+++ src/tiy_homework_fizz_buzz_clojure/core.clj
@@ -1,22 +1,25 @@
 (ns tiy-homework-fizz-buzz-clojure.core)
 
-(defn -main[])
 
-  (defn fizz-buzz[start end]
-    (for [x (range start end)]
-        (cond
-          (= (mod x 15) 0) (println "FizzBuzz")
-          (= (mod x 5) 0) (println "Buzz")
-          (= (mod x 3) 0) (println "Fizz")
-                :else (println x)))
-      )
+
+(defn fizz-buzz[start end]
+  (for [x (range start end)]
+    (cond
+        (and (= (mod x 5) 0)
+             (= (mod x 3) 0)) (str "FizzBuzz")
+        (= (mod x 5) 0) (str "Buzz")
+        (= (mod x 3) 0) (str "Fizz")
+        :else (str x))))
+
 
 
     ;For multiples of three print "Fizz" instead of the number.
     ;For the multiples of five it print "Buzz".
     ;For numbers which are multiples of both three and five print "FizzBuzz".
 
+(defn -main[]
+  (print (fizz-buzz 1 101)))
 
 (-main)
 
-(fizz-buzz 1 101)
+

a8a32e28eefd20ed9588ca394b8a00f134a7152a
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-10 15:47:27 -0400

    almost there

--- src/tiy_homework_fizz_buzz_clojure/core.clj
+++ src/tiy_homework_fizz_buzz_clojure/core.clj
@@ -1,3 +1,22 @@
 (ns tiy-homework-fizz-buzz-clojure.core)
 
+(defn -main[])
 
+  (defn fizz-buzz[start end]
+    (for [x (range start end)]
+        (cond
+          (= (mod x 15) 0) (println "FizzBuzz")
+          (= (mod x 5) 0) (println "Buzz")
+          (= (mod x 3) 0) (println "Fizz")
+                :else (println x)))
+      )
+
+
+    ;For multiples of three print "Fizz" instead of the number.
+    ;For the multiples of five it print "Buzz".
+    ;For numbers which are multiples of both three and five print "FizzBuzz".
+
+
+(-main)
+
+(fizz-buzz 1 101)
